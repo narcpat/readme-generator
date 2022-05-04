1649a14c3a5135ea7733f32dc442127c0906325b
narcpat <narcissearts@gmail.com>
2022-05-04 16:28:57 -0400

    changed question variable to promptUser in order to use Inquirer functions.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// To prompt the questions
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?",
+      validate: nameInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter the title of your project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a brief description of your project: ",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
