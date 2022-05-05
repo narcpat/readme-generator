c6e3f66e5a89884d75d4c0639ee17d14eee57519
narcpat <narcissearts@gmail.com>
2022-05-05 11:13:14 -0400

    started working on logic to get the badge and license link to work

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `
+  ![badge](https://img.shields.io/github/license/<Github-Username>/<Repository>)
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,13 +12,22 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+
+  return `
+    ${renderLicenseBadge}
+    ${renderLicenseLink}
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
-
+    
   ## Description
   ${data.description}
 
@@ -33,7 +46,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  ${data.license}
+  ${renderLicenseSection.license}
 
   ## Contributing
   ${data.contributing}
