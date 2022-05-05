5355a8c027e368a76f0e5e00a7442367aa754738
narcpat <narcissearts@gmail.com>
2022-05-05 12:52:51 -0400

    cleaned up the prompts and added then statement to start storing userinput data

--- index.js
+++ index.js
@@ -5,72 +5,71 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 // To prompt the questions
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the name of your project?",
-      validate: titleInput => {
-        if (titleInput) {
-          return true;
-        } else {
-          console.log("You need to enter a project name!");
-          return false;
-        }
+const promptUser = () =>
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project name!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a brief description of your project: ",
-      validate: descriptionInput => {
-        if (descriptionInput) {
-          return true;
-        } else {
-          console.log("You need to enter a project description!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description of your project: ",
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project description!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Describe the installation process (if applicable): ",
-      default: false,
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Provide instructions and examples for use: ",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Please select the appropriate license for your project: ",
-      choices: ["GNU", "MIT", "Mozilla", "Boost", "Apache", "The Unlicense"],
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "Please provide guidelines for contributors (optional): ",
-      default: false,
-    },
-    {
-      type: "input",
-      name: "tests",
-      message:
-        "If you have written tests, provide examples on how to run them (optional): ",
-      default: false,
-    },
-    {
-      type: "input",
-      name: "questions",
-      message: "Provide your email address for feedback and inquiries: ",
-      default: false,
-    },
-  ]);
-};
+      {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process (if applicable): ",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use: ",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Please select the appropriate license for your project: ",
+        choices: ["GNU", "MIT", "Mozilla", "Boost", "Apache", "The Unlicense"],
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Please provide guidelines for contributors (optional): ",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message:
+          "If you have written tests, provide examples on how to run them (optional): ",
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Provide your email address for feedback and inquiries: ",
+      },
+    ])
+    .then(userAnswers => {
+      console.log(userAnswers.description);
+    });
 
 promptUser();
 // TODO: Create a function to write README file
