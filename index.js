5bda863b68708c66455ea858a5bcae80d3fd1031
narcpat <narcissearts@gmail.com>
2022-05-05 18:59:38 -0400

    finished write file function

--- index.js
+++ index.js
@@ -6,81 +6,74 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 // To prompt the questions
 const promptUser = () =>
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the name of your project?",
-        validate: titleInput => {
-          if (titleInput) {
-            return true;
-          } else {
-            console.log("You need to enter a project name!");
-            return false;
-          }
-        },
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?",
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("You need to enter a project name!");
+          return false;
+        }
       },
-      {
-        type: "input",
-        name: "description",
-        message: "Provide a brief description of your project: ",
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log("You need to enter a project description!");
-            return false;
-          }
-        },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a brief description of your project: ",
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("You need to enter a project description!");
+          return false;
+        }
       },
-      {
-        type: "input",
-        name: "installation",
-        message: "Describe the installation process (if applicable): ",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions and examples for use: ",
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Please select the appropriate license for your project: ",
-        choices: ["GNU", "MIT", "Mozilla", "Boost", "Apache", "The Unlicense"],
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: "Please provide guidelines for contributors (optional): ",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message:
-          "If you have written tests, provide examples on how to run them (optional): ",
-      },
-      {
-        type: "input",
-        name: "questions",
-        message: "Provide your email address for feedback and inquiries: ",
-      },
-    ])
-    .then(userAnswers => {
-      console.log(userAnswers.description);
-    });
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Describe the installation process (if applicable): ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use: ",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please select the appropriate license for your project: ",
+      choices: ["GNU", "MIT", "Mozilla", "Boost", "Apache", "The Unlicense"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Please provide guidelines for contributors (optional): ",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "If you have written tests, provide examples on how to run them (optional): ",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Provide your email address for feedback and inquiries: ",
+    },
+  ]);
 
 // TODO: Create a function to initialize app
 function init() {
   // Ask user questions and generate the answers
   promptUser()
-    .then(userAnswers => {
-      return generateContent(userAnswers);
-    })
-    // write the new README to Dist
     .then(readmePageBuild => {
-      return writeToFile("./dist/README.md", generateContent);
+      console.log(readmePageBuild);
+      return writeToFile("./dist/README.md", generateMarkdown(readmePageBuild));
       console.log("README successfully created");
     })
     .catch(err => {
@@ -89,7 +82,11 @@ function init() {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) throw err;
+  });
+}
 
 // Function call to initialize app
 init();
